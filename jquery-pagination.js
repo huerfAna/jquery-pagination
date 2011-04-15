1bf20ec5bf858594bdd9bd8d199dab8c6c978a66
Jonas Geiregat <jonas@geiregat.org>
2011-04-15 15:01:17 +0200

    Hide items, then show only items based on the item_pointer.

--- jquery-pagination.js
+++ jquery-pagination.js
@@ -2,6 +2,9 @@ $.fn.PageIt = function(options) {
     var lenght,
         page_pointer, // starts at 0
         item_count, // Counts the dataElement
+        items_a_page = 10, // Number of items on a page
+        page_count, // Total of pages
+        paginator_template = '<div id="paginator"><a href="#">{{ page_number }}</a></div>';
         defaults = {
             dataElement: '#dataElement li',
             navElement: 'div',
@@ -12,30 +15,52 @@ $.fn.PageIt = function(options) {
     buildUI();
 
     function init() {
-       countPages();
+       hideAllPageItems();
+
        page_pointer = 0;  
+
+
+       if($(opts.dataElement)) {
+            item_count = $(opts.dataElement).length;
+       }
+
+       page_count = Math.ceil(item_count / items_a_page); 
+       item_pointer = page_pointer * page_count;
+    }
+    
+    // Hide all items
+    function hideAllPageItems() {
+        $('#dataElement li').css({display: 'none'});
     }
 
-    // add class hidden to none page items
-    function slice() {
-        var page_count = Math.ceil(item_count / 5); 
+    function moveToPage() {
 
-        item_pointer = page_pointer * page_count;
-        item_pointer++;
+        var show_page_items = function(i_p) {
+            var $el;
+            i_p++;
+            $el = $('#dataElement li:nth-child(' + i_p + ')');
 
-        //lus oplossen via recursie
-        $('#dataElement li:nth-child(' + item_pointer + ')').css({display: 'none'}).next().css({display: "none"});
+            $el.css({display: 'block'});
+
+            return i_p;
+        };
+
+
+        for(var i=0; i < items_a_page; i++) { 
+            item_pointer = show_page_items(item_pointer);
+        }
     }
 
     function buildUI() {
-        slice();
+        buildPaginator();
+        /*buildSlider();*/
+        moveToPage();
     }
 
-    function countPages() {
-        if($(opts.dataElement)) {
-            item_count = $(opts.dataElement).length;
-        }
+    function buildPaginator() {
+         
     }
+
 
     // Count total pages
     // slice the pages function
