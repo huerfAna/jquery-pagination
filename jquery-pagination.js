8d8d7a6a607b849929ac2a046422edb504f377fd
Jonas Geiregat <jonas@geiregat.org>
2012-01-29 23:42:08 +0100

    Removed init param

--- jquery-pagination.js
+++ jquery-pagination.js
@@ -4,7 +4,6 @@
             item_pointer, // starts at 0, points to the first item of the current page. 
             page_count, // Number of pages
             self = $(this), // refers to the data container
-            init = True, //Sad work around to find out if this is the first time the list should be displayed (read animated)
 
             defaults = {
                 data_element: 'li', 
