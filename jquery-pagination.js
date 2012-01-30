5a736b6743f5d241f266eb73bcb020fe36f3d505
Jonas Geiregat <jonas@geiregat.orgmkdir>
2012-01-30 13:24:16 +0100

    Changed slideOut to slideDown

--- jquery-pagination.js
+++ jquery-pagination.js
@@ -49,7 +49,7 @@
         function removeItem(index) {
             $(opts.data_element)
                 .get(index)
-                    .slideOut()
+                    .slideDown()
                     .remove();
         }
 
